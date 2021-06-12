643e8a1b4a7536760215045744bbd929cfdf9c85
Qiming Liu <qooroo@gmail.com>
2021-06-12 21:09:11 +0100

    select ga numbers on focus

--- src/GestationalAgeForm.tsx
+++ src/GestationalAgeForm.tsx
@@ -1,18 +1,28 @@
 
+const handleFocus = (event: { target: { select: () => any; }; }) => event.target.select();
+
 export const GestationalAgeForm: React.FC<{ label: string, weeksHook: () => number, onWeeksChange: any, daysHook: () => number, onDaysChange: any }> = (x) => {
-  
+
   const weeks = x.weeksHook()
   const days = x.daysHook()
-  
+
   return (
+    <div>
+      <p className="label">{x.label}</p>
       <div>
-        <p className="label">{x.label}</p>
-        <div>
-          <input className="gest-input" onChange={x.onWeeksChange} value={weeks}></input>
-          Weeks
-          <input className="gest-input" onChange={x.onDaysChange} value={days}></input>
-          Days
-        </div>
+        <input
+          className="gest-input"
+          onFocus={handleFocus}
+          onChange={x.onWeeksChange}
+          value={weeks}></input>
+        Weeks
+        <input
+          className="gest-input"
+          onFocus={handleFocus}
+          onChange={x.onDaysChange}
+          value={days}></input>
+        Days
       </div>
-    );
-  }
+    </div>
+  );
+}
